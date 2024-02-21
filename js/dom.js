3c7b4bba48dbd534a7a15a01f111c98614f9b6ac
israt6898 <israt6898@gmail.com>
2024-02-21 20:07:51 +0600

    finish append child

--- js/dom.js
+++ js/dom.js
@@ -1,25 +1,43 @@
-const allseat = document.getElementsByClassName('select-btn');
- let totalSeat = 8;
- let addSeat= 0;
+const allDiv= document.getElementsByClassName('select-btn');
 
- for(const seat of allseat){
-  
-    seat.addEventListener('click', function(e){
-        totalSeat=totalSeat-1;
-        addSeat=addSeat+1;
-        document.getElementById("minus-seat").innerText=totalSeat;
-        document.getElementById("add-seat").innerText=addSeat;
-        
+for(const div of allDiv){
+   div.addEventListener("click", function(event){
+    const seat = event.target.innerText;
+    const seatClass='Economic';
+    const price = 550;
+    const selectedContainer = document.getElementById("select-container");
+    const tr = document.createElement("tr");
+     
+    const th1= document.createElement("th");
+    const th2= document.createElement("th");
+    const th3= document.createElement("th");
 
-      
-       
-        
-    })
- }
+    th1.innerText = seat;
+    th2.innerText= seatClass;
+    th3.innerText = price;
+    tr.appendChild(th1);
+    tr.appendChild(th2);
+    tr.appendChild(th3);
+    selectedContainer.appendChild(tr);
 
-function showDivElement(){
-    const showSection =document.getElementById('showDiv');
-    showSection.classList.remove('hidden');
-
-
+   })
 }
+
+
+
+
+
+
+
+function getConvertValue(id){
+    const seat =document.getElementById(id).innerText;
+    const convertedSeat = parseInt(seat);
+    return convertedSeat;
+}
+
+
+
+
+ 
+
+
